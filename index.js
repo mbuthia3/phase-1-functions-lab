b212221ecf9563b7cab65f557903dfdab371a91a
victor <victor.mbuthia@student.moringaschool.com>
2022-08-17 14:06:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const scuberHeadquaters = 42;
+
+function distanceFromHqInBlocks(pickUpLocation) {
+    const distanceInBlocks = pickUpLocation > scuberHeadquaters ? pickUpLocation - scuberHeadquaters : scuberHeadquaters - pickUpLocation;
+    return distanceInBlocks;
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+    let distanceInBlocks = distanceFromHqInBlocks(pickUpLocation);
+    return distanceInBlocks *264;
+}
+
+function distanceTravelledInFeet(startingBlock , endingBlock) {
+    let distaceTravelled = endingBlock > startingBlock ? (endingBlock - startingBlock)*264 : (startingBlock - endingBlock)*264;
+    return distaceTravelled;
+}
+
+function calculatesFarePrice(startingBlock , endingBlock) {
+    let distanceInFeet = distanceTravelledInFeet(startingBlock , endingBlock);
+    if(distanceInFeet < 400) { 
+        return 0
+} else if(distanceInFeet > 400 && distanceInFeet < 2000) {
+    return(distanceInFeet - 400)*0.02;
+} else if(distanceInFeet > 2000) {
+    if(distanceInFeet > 2500) {
+        return 'cannot travel that far'
+    }
+    return 25;
+}
+}
